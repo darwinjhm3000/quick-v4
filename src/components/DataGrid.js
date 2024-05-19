22f55ec202bb9ade9b317387d82e27cd7fc6c9cf
Darwin.Wigilab <dhurtado@wigilabs.com>
2024-05-19 10:58:03 -0500

    initial commit

--- src/components/DataGrid.js
+++ src/components/DataGrid.js
@@ -2,12 +2,17 @@ import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 
 const columns = [
-  { field: 'name', headerName: 'Name', width: 150 },
+  { field: 'id', headerName: 'ID', width: 50 },
+  { field: 'name', headerName: 'Name', width: 100 },
+  { field: 'location', headerName: 'Localización', width: 100 },
 ];
 
 export default function DataTable({ data }) {
-  const rows = data.map((item, index) => ({ id: index, name: item.name }));
-
+  const rows = data.map((item, index) => ({
+    id: index,
+    name: item.name,
+    location: item.location,
+  }));
 
   return (
     <div style={{ height: 400, width: '20%' }}>
